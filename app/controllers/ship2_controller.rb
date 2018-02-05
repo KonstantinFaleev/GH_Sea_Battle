91fa4bb508c970bd304e5eb15889e916f941df20
alextriam <alextriam@gmail.com>
2018-02-05 21:33:02 +0200

    all ships

--- app/controllers/ship2_controller.rb
+++ app/controllers/ship2_controller.rb
@@ -7,33 +7,99 @@ class Ship2Controller < ApplicationController
   def show
     @player_one = Array.new(10).map! { Array.new(10) }
 
-    @player_one.each_index do |i|
-      @player_one[i].each_index do |j|
-        @player_one[i][j] = [i, '_', j]
+    @player_one.each_index do |x_axis|
+      @player_one[x_axis].each_index do |y_axis|
+        @player_one[x_axis][y_axis] = [x_axis, '_', y_axis]
       end
     end
 
     if rand(0..1) == 1
-      horizontal_axis = rand(0..6)
-      vertical_axis = rand(0..9)
-      border = horizontal_axis + 4
-      while horizontal_axis < border
-        @player_one[horizontal_axis + 1][vertical_axis] = 'Z'
-        horizontal_axis += 1
+      y_axis = rand(0..5)
+      x_axis = rand(0..9)
+      border = y_axis + 4
+      while y_axis < border
+        @player_one[y_axis + 1][x_axis] = '4'
+        y_axis += 1
       end
     else
-      horizontal_axis = rand(0..9)
-      vertical_axis = rand(0..6)
-      border = vertical_axis + 4
-      while vertical_axis < border
-        @player_one[horizontal_axis][vertical_axis + 1] = 'Z'
-        vertical_axis += 1
+      x_axis = rand(0..9)
+      y_axis = rand(0..5)
+      border = y_axis + 4
+      while y_axis < border
+        @player_one[x_axis][y_axis + 1] = '4'
+        y_axis += 1
       end
     end
+2.times do
+    if rand(0..1) == 1 # three ship
+      y_axis = rand(0..6)
+      x_axis = rand(0..9)
+      border = y_axis + 3
+      while y_axis < border
+        @player_one[y_axis + 1][x_axis] = '3'
+        y_axis += 1
+      end
+    else
+      x_axis = rand(0..9)
+      y_axis = rand(0..6)
+      border = y_axis + 3
+      while y_axis < border
+        @player_one[x_axis][y_axis + 1] = '3'
+        y_axis += 1
+      end
+    end
+end
+
+    3.times do
+      if rand(0..1) == 1 # two ship
+        y_axis = rand(0..7)
+        x_axis = rand(0..9)
+        border = y_axis + 2
+        while y_axis < border
+          @player_one[y_axis + 1][x_axis] = '2'
+          y_axis += 1
+        end
+      else
+        x_axis = rand(0..9)
+        y_axis = rand(0..7)
+        border = y_axis + 2
+        while y_axis < border
+          @player_one[x_axis][y_axis + 1] = '2'
+          y_axis += 1
+        end
+      end
+
+    end
+
+    4.times do
+      if rand(0..1) == 1 # three ship
+        y_axis = rand(0..8)
+        x_axis = rand(0..9)
+        border = y_axis + 1
+        while y_axis < border
+          @player_one[y_axis + 1][x_axis] = '1'
+          y_axis += 1
+        end
+      else
+        x_axis = rand(0..9)
+        y_axis = rand(0..8)
+        border = y_axis + 1
+        while y_axis < border
+          @player_one[x_axis][y_axis + 1] = '1'
+          y_axis += 1
+        end
+      end
+    end
+
+
     render 'show'
   end
 
   def set_ship
-
   end
+  def free_cell (axis_x, axis_y)
+    if (0..9).include?(axis_x) and (0..9).include?(axis_y)
+
+    end
+      end
 end
