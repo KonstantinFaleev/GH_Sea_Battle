723314a7b031af3793549ef33c0a2f6312af0c39
KonstantinFaleev <konstantin.faleev@gmail.com>
2018-02-08 16:31:52 +0200

    class board

---
+++ app/sea_battle/board.rb
@@ -0,0 +1,17 @@
+class Board
+
+  def add_board
+    if self.new_record?
+      self.available_ships = [4, 3, 3, 2, 2, 2, 1, 1, 1, 1]
+      grid = []
+      10.times do
+        row = []
+        10.times do
+          row << [0, 0]
+        end
+        grid << row
+      end
+      self.grid = grid
+    end
+  end
+end
