11f9ed58fb4ad575a1b634e90fa190510950f2ca
alextriam <alextriam@gmail.com>
2018-02-12 22:52:18 +0200

    done set ship, only vertical

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
 
   resources :ship2
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
- resources :ship
+  resources :ship
 
 end
